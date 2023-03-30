9f98f917ca3043b6aa73a34a6a6ad7986949566c
Oleh <oleh.pshenychnyi@showmax.com>
2023-03-30 21:58:41 +0200

    Tests.

--- src/test/java/ValidateParamsTests.java
+++ src/test/java/ValidateParamsTests.java
@@ -8,7 +8,6 @@ public class ValidateParamsTests extends BaseTest {
     @Test
     @DisplayName("Run command with empty question.")
     void testEmptyQuestion() {
-        // Empty string.
         Integer exitCode = this.commandLine.execute("");
         assertEquals(2, exitCode);
     }
@@ -16,8 +15,14 @@ public class ValidateParamsTests extends BaseTest {
     @Test
     @DisplayName("Run command with wrong temperature value.")
     void testWrongTemperature() {
-        // Empty string.
         Integer exitCode = this.commandLine.execute("List all files as a table", "-t=3");
+        assertEquals(2, exitCode);
+    }
+
+    @Test
+    @DisplayName("Run command with wrong temperature value.")
+    void testWrongModel() {
+        Integer exitCode = this.commandLine.execute("List all files as a table", "-m=mmmm");
         assertEquals(2, exitCode);
     }
 }
